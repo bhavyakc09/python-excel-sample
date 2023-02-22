007b5b50819055dcc5ddca020eb8a2b2b33a398f
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-22 16:54:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,17 @@ def main():
     ws = wb.active
     cell_reference = sys.argv[1]
     print(f'Processing cell {cell_reference}')
+    
+    while True:
+    try:
+        sys.stdout.flush()
+        print("Please enter the cell reference (e.g. A1): ")
+        cell_reference = input()
+        if not cell_reference:
+            raise ValueError('Cell reference cannot be empty')
+        break
+    except (EOFError, ValueError) as e:
+        print(f'Error: {e}')
 
 if __name__ == '__main__':
     main()
