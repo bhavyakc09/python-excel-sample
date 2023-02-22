a785a859913d8c248309cf2dc6929d956d371cdb
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-22 17:28:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,16 @@
 import openpyxl
-import sys
+import os
 
 def main():
-    if len(sys.argv) > 1:
-        cell_reference = sys.argv[1]
-        print(f'Processing cell {cell_reference}')
-    else:
-        while True:
-            try:
-                print("Please enter the cell reference (e.g. A1): ")
-                cell_reference = input()
-                if not cell_reference:
-                    raise ValueError('Cell reference cannot be empty')
-                break
-            except (EOFError, ValueError) as e:
-                print(f'Error: {e}')
-
     wb = openpyxl.load_workbook('input_data.xlsx')
     ws = wb.active
-    print(f'Value in cell {cell_reference}: {ws[cell_reference].value}')
+    cell_reference = os.environ.get('CELL_REFERENCE')
+    if not cell_reference:
+        raise ValueError('CELL_REFERENCE environment variable is not set')
+    print(f'Processing cell {cell_reference}')
+    
+    cell_value = ws[cell_reference].value
+    print(f'Value in cell {cell_reference}: {cell_value}')
 
 if __name__ == '__main__':
     main()
