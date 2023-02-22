e07fff8f1558f58f7bb96186fb9010078b5666bb
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-22 15:41:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,13 @@ def main():
         return
     cell_reference = sys.argv[1]
 
-workbook = openpyxl.load_workbook('input_data.xlsx')
-worksheet = workbook.active
-print("Before input prompt")
-cell_reference = input("Please enter the cell reference (e.g. A1): ")
-print("After input prompt")
-print(cell.value)
+    workbook = openpyxl.load_workbook('input_data.xlsx')
+    worksheet = workbook.active
+    print("Before input prompt")
+    cell_reference = input("Please enter the cell reference (e.g. A1): ")
+    print("After input prompt")
+    cell = worksheet[cell_reference]
+    print(cell.value)
 
+if __name__ == "__main__":
+    main()
