8f2827e1fcd86567a88394447460b1c07c3c722a
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-22 16:38:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-import openpyxl
 import sys
+from openpyxl import load_workbook
 
-def main():
-    wb = openpyxl.load_workbook('input_data.xlsx')
+def main(cell_reference='A1'):
+    wb = load_workbook('input_data.xlsx')
     ws = wb.active
-    cell_reference = sys.argv[1] if len(sys.argv) > 1 else 'A1'
     print(f'Processing cell {cell_reference}')
+    # do something with the worksheet and cell reference here
 
 if __name__ == '__main__':
-    main()
+    cell_reference = sys.argv[1] if len(sys.argv) > 1 else 'A1'
+    main(cell_reference)
 
