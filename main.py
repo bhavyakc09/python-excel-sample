013de00686c1e32e7579fbabf3e7fa8fd2c07710
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-22 16:01:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,8 @@
-import sys
 import openpyxl
 
-def main():
-    # parse command line arguments
-    if len(sys.argv) < 2:
-        print('Please provide a cell reference (e.g. A1) as a command line argument')
-        return
-    cell_reference = sys.argv[1]
+workbook = openpyxl.load_workbook('input_data.xlsx')
+worksheet = workbook.active
 
-    workbook = openpyxl.load_workbook('input_data.xlsx')
-    worksheet = workbook.active
-    print("Before input prompt")
-    cell_reference = input('Please enter the cell reference (e.g. A1): ') or 'A1'
-    print("After input prompt")
-    cell = worksheet[cell_reference]
-    print(cell.value)
-
-if __name__ == "__main__":
-    main()
+cell_reference = input("Please enter the cell reference (e.g. A1): ")
+cell = worksheet[cell_reference]
+print(cell.value)
