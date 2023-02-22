d4cc58e072e2c1c6af4fe70636143a84e2d13467
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-22 16:31:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ import openpyxl
 def main():
     wb = load_workbook('input_data.xlsx')
     ws = wb.active
+    
+     if len(sys.argv) > 1:
+        cell_reference = sys.argv[1]
+    else:
+        cell_reference = 'A1'
+        
     while True:
         try:
             cell_reference = input('Please enter the cell reference (e.g. A1): ')
@@ -13,12 +19,4 @@ def main():
             print(f'Error: {e}')
     print(f'Processing cell {cell_reference}')
 
-    if len(sys.argv) > 1:
-        cell_reference = sys.argv[1]
-    else:
-        cell_reference = 'A1'
-
-    print(f"Processing cell reference: {cell_reference}")
-
-if __name__ == "__main__":
-    main()
+   
