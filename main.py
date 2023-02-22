032f8723a89cd92162390f41e0ed061449feb4d6
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-22 16:52:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,7 @@ import sys
 def main():
     wb = openpyxl.load_workbook('input_data.xlsx')
     ws = wb.active
-    while True:
-        try:
-            cell_reference = input('Please enter the cell reference (e.g. A1): ')
-            if not cell_reference:
-                raise ValueError('Cell reference cannot be empty')
-            break
-        except (EOFError, ValueError) as e:
-            print(f'Error: {e}')
+    cell_reference = sys.argv[1]
     print(f'Processing cell {cell_reference}')
 
 if __name__ == '__main__':
