840db3c54ce4fbb9e0bd55c99fc526a7c65b4612
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-22 11:39:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
-import argparse
+import sys
 import openpyxl
 
-parser = argparse.ArgumentParser(description='Get value from cell in Excel file.')
-parser.add_argument('cell_reference', help='The cell reference (e.g. A1)')
-args = parser.parse_args()
-
-input_file_path = 'input_data.xlsx'
+if len(sys.argv) > 1:
+    cell_reference = sys.argv[1]
+else:
+    cell_reference = input('Please enter the cell reference (e.g. A1): ')
 
 workbook = openpyxl.load_workbook('input_data.xlsx')
 worksheet = workbook.active
-
-cell = worksheet[args.cell_reference]
+cell = worksheet[cell_reference]
 print(cell.value)
 
