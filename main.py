c2c705e0b85d4d5c1cef527d9356bd8373b01980
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-22 17:41:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,20 +3,9 @@ import os
 os.environ['CELL_REFERENCE'] = 'A1'
 
 def main():
-    # Get the cell reference from the environment variable
-    cell_reference = os.environ.get('CELL_REFERENCE')
-
-    if cell_reference is None:
-        raise ValueError('CELL_REFERENCE environment variable is not set')
-
-    # Load the workbook
-    wb = load_workbook(filename='example.xlsx')
-
-    # Get the active worksheet
-    ws = wb.active
-
-    # Get the value in the cell
+    cell_reference = input("Please enter the cell reference (e.g. A1): ")
     cell_value = ws[cell_reference].value
+    print(f"Value in cell {cell_reference}: {cell_value}")
 
-    # Print the cell value
-    print(f'Value in cell {cell_reference}: {cell_value}')
+if __name__ == '__main__':
+    main()
