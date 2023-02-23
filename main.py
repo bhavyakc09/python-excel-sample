dba5c0231740e2682d1486225ce31f1db7bc494c
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-23 16:47:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 def main():
     print("Main function called")
     input_file = sys.argv[1]
@@ -11,4 +13,5 @@ def main():
     filename = sys.argv[1]
     wb = openpyxl.load_workbook(filename)
     ws = wb.active
+    time.sleep(1) # wait for 1 second
     cell_reference = input("Please enter the cell reference (e.g. A1): ")
