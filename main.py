e2ab295357277303f52c9f37a301bf29bf6a6f48
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-23 15:11:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,21 +2,18 @@ import sys
 import openpyxl
 
 def main():
+    print("Main function called")
     if len(sys.argv) < 2:
         print("Please provide the name of the input file as a command-line argument")
         return
     
-    filename = sys.argv[1]
-    wb = openpyxl.load_workbook(filename)
+    input_file = sys.argv[1]
+    wb = openpyxl.load_workbook(input_file)
     ws = wb.active
-    
-    # Prompt the user to enter a cell reference
     cell_reference = input("Please enter the cell reference (e.g. A1): ")
     cell_value = ws[cell_reference].value
-    
-    print(f"The value of cell {cell_reference} is: {cell_value}")
+    print(f"The value of cell {cell_reference} is {cell_value}")
 
 
 if __name__ == '__main__':
     main()
-
