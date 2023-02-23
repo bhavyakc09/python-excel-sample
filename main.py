ae1eaac139615644e67b1983cde657cdccd5b211
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-23 14:49:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import sys
+import openpyxl
 
 def main():
     if len(sys.argv) < 2:
-        print("Please provide a cell reference as an argument (e.g. A1)")
+        print("Please provide the name of the input file as a command-line argument")
         return
     
-    cell_reference = sys.argv[1]
+    filename = sys.argv[1]
+    wb = openpyxl.load_workbook(input_data.xlsx)
+    ws = wb.active
+    cell_reference = ws['A1'].value  # Change this to the cell reference that contains the input
+    
     # Rest of your code here
 
 if __name__ == '__main__':
