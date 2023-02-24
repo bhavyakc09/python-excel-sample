38d90a42a4864b4155e607054f1851fec6e7b5a2
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-24 15:41:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ def main():
     if len(sys.argv) < 2:
         print("Please provide the name of the input file as a command-line argument")
         return
+    
+    if sys.version_info[0] < 3:
+        input_func = raw_input
+    else:
+        input_func = input
 
     input_file = sys.argv[1]
     print("Input file is:", input_file)
@@ -15,7 +20,7 @@ def main():
 
     print("Waiting for input...")
     prompt = "Please enter the cell reference (e.g. A1): "
-    cell_reference = input(prompt)
+    cell_reference = input_func(prompt)
     cell_value = ws[cell_reference].value
     print(f"The value of cell {cell_reference} is: {cell_value}")
 
