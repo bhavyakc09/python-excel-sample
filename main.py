045bd2df72526bf064daee18bda1b30039bbfd60
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-24 13:15:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-import time
+import sys
+import openpyxl
 
 def main():
-    print("Main function called")
-    input_file = sys.argv[1]
-
     if len(sys.argv) < 2:
         print("Please provide the name of the input file as a command-line argument")
         return
     
+    input_file = sys.argv[1]
     print("Input file is:", input_file)
 
     filename = sys.argv[1]
     wb = openpyxl.load_workbook(filename)
     ws = wb.active
-    time.sleep(1) # wait for 1 second
+
     cell_reference = input("Please enter the cell reference (e.g. A1): ")
+    cell_value = ws[cell_reference].value
+    print(f"The value of cell {cell_reference} is: {cell_value}")
