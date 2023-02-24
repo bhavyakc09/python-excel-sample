5f1aabcbc23fd01d1332e459ca8667314f396eec
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-24 16:19:34 +0530

    Update get_input.py

--- get_input.py
+++ get_input.py
@@ -2,9 +2,9 @@ import sys
 
 def get_input(prompt):
     try:
-        return raw_input(prompt)
-    except NameError:
         return input(prompt)
+    except NameError:
+        return raw_input(prompt)
 
 cell_reference = get_input("Please enter the cell reference (e.g. A1): ")
 print(cell_reference)
