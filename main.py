8c8b61b36f3b6ae04fdcf5c68b51290a34079802
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-24 13:21:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import sys
 import openpyxl
 
 def main():
-    if len(sys.argv) < 3:
-        print("Please provide the name of the input file and cell reference as command-line arguments")
+    if len(sys.argv) < 2:
+        print("Please provide the name of the input file as a command-line argument")
         return
     
     input_file = sys.argv[1]
@@ -13,6 +13,7 @@ def main():
     wb = openpyxl.load_workbook(filename)
     ws = wb.active
 
-    cell_reference = sys.argv[2]
+    with open('cell_reference.txt', 'r') as f:
+        cell_reference = f.read().strip()
     cell_value = ws[cell_reference].value
     print(f"The value of cell {cell_reference} is: {cell_value}")
